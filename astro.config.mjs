27e7c07822ac915ce5653f6a03e98a08921e1538
Patric Jenni <patric@jenni-mail.ch>
2026-01-14 21:44:17 +0000

    Fix link paths when deploying

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,15 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/kochrezepte" : "/",
+  base: process.env.NODE_ENV === "production" ? "/kochrezepte/" : "/",
+  vite: {
+    server: {
+      host: "0.0.0.0",
+      hmr: {
+        protocol: "ws",
+        host: "localhost",
+        port: 4321,
+      },
+    },
+  },
 });
