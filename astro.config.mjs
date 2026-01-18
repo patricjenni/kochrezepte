83ee0723e3db73e76ec03f5791a99d642c781ad1
Patric Jenni <patric@jenni-mail.ch>
2026-01-18 09:28:16 +0000

    Fix hot reloading not working in devcontainer

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,16 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 4321,
+  },
   base: process.env.NODE_ENV === "production" ? "/kochrezepte/" : "/",
   vite: {
     server: {
-      host: "0.0.0.0",
-      hmr: {
-        protocol: "ws",
-        host: "localhost",
-        port: 4321,
+      watch: {
+        usePolling: true,
+        interval: 100,
       },
     },
   },
